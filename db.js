6be707d7adcde4d526a94e6bb8d09e127b6fce44
Umid <umid.ismayilov01@gmail.com>
2022-01-12 11:37:02 +0400

    first commit

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 // const db = mysql.createConnection({
-//     host: "173.212.224.244",
-//     user: "newuser",
-//     password: "P4ss3v1l@l1mklM@K",
-//     database : 'limak_db',
+//     host: process.env.DB_HOST,
+//     user:  process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database : process.env.DB_DATABASE,,
 //     port : 3306,
 // });
 //
@@ -11,6 +11,7 @@ const mysql = require('mysql');
 //     if (err) throw err;
 //     console.log('Connected to MySQL Server!');
 // });
+
 
 const db = mysql.createPool({
     connectionLimit : 100,
