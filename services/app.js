05e56153c1c9a7edb5b69365b28e400c0683b062
Umid <umid.ismayilov01@gmail.com>
2022-01-24 11:53:37 +0400

    meta service connect

--- services/app.js
+++ services/app.js
@@ -1,8 +1,15 @@
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const app = () => {
+const app = (req, res) => {
     const dotenv = require('dotenv').config();
     global.view = require('../helpers').view;
     global.db = require('../db')
+    const bodyParser = require('body-parser')
+    var cors = require('cors')
+    req.app.use(cors())
+    req.app.use(bodyParser.json())
+    req.app.use(bodyParser.urlencoded({extended: true}))
 }
 
 module.exports = app;
