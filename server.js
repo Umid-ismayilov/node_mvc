1f251e747f5e95e4194c6d6c2f9bcda3d638114e
Umid <umid.ismayilov01@gmail.com>
2022-01-24 11:59:11 +0400

    clear

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var app     = require('express')();
 var http    = require('http').createServer(app);
 require('./services/socket').connection(http)
-require('./services/app')(app);
+require('./services/app')();
 require('./services/routers')(app);
+
+const bodyParser = require('body-parser')
+var cors = require('cors')
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 const PORT  = process.env.PORT || 5000
 
--- services/app.js
+++ services/app.js
@@ -5,11 +5,7 @@ const app = (app) => {
     const dotenv = require('dotenv').config();
     global.view = require('../helpers').view;
     global.db = require('../db')
-    const bodyParser = require('body-parser')
-    var cors = require('cors')
-    app.use(cors())
-    app.use(bodyParser.json())
-    app.use(bodyParser.urlencoded({extended: true}))
+
 }
 
 module.exports = app;
